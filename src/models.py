c05d4a0a6efe9c5f85062d5257357ccde98e4437
zimonitrome <zimonitrome@gmail.com>
2020-05-26 01:31:58 +0200

    New models (32x16 -> 256->128)

--- src/models.py
+++ src/models.py
@@ -1,36 +1,53 @@
 import torch.nn as nn
-import torch.nn.functional as F
-
 
 class Generator(nn.Module):
 
     def __init__(self):
         super(Generator, self).__init__()
-        # Convolutional Layer: 100x1x1 -> 512x1x2, F=(1,2), S=1, P=0
-        self.conv1 = nn.ConvTranspose2d(100, 512, (1, 2), 1, 0, bias=False)
-        self.conv1_bn = nn.BatchNorm2d(512)
+        
+        self.conv = nn.Sequential(
+            # Convolutional Layer: 100x1x1 -> 512x1x2, F=(1,2), S=1, P=0
+            nn.ConvTranspose2d(100, 512, (1, 2), 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
 
-        # Convolutional Layer: 512x1x2 -> 256x2x4, F=4, S=2, P=1
-        self.conv2 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
-        self.conv2_bn = nn.BatchNorm2d(256)
+            # Convolutional Layer: 512x1x2 -> 256x2x4, F=4, S=2, P=1
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
 
-        # Convolutional Layer: 256x2x4 -> 128x4x8, F=4, S=2, P=1
-        self.conv3 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
-        self.conv3_bn = nn.BatchNorm2d(128)
+            # Convolutional Layer: 256x2x4 -> 128x4x8, F=4, S=2, P=1
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
 
-        # Convolutional Layer: 128x4x8 -> 64x8x16, F=4, S=2, P=1
-        self.conv4 = nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)
-        self.conv4_bn = nn.BatchNorm2d(64)
+            # Convolutional Layer: 128x4x8 -> 64x8x16, F=4, S=2, P=1
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
 
-        # Convolutional Layer: 64x8x16 -> 4x16x32, F=4, S=2, P=1
-        self.conv5 = nn.ConvTranspose2d(64, 4, 4, 2, 1, bias=False)
+            # Convolutional Layer: 64x8x16 -> 32x16x32, F=4, S=2, P=1
+            nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
 
+            # Convolutional Layer: 32x8x16 -> 16x32x64, F=4, S=2, P=1
+            nn.ConvTranspose2d(32, 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+
+            # Convolutional Layer: 16x32x64 -> 8x64x128, F=4, S=2, P=1
+            nn.ConvTranspose2d(16, 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+
+            # Convolutional Layer: 8x64x128 -> 4x128x256, F=4, S=2, P=1
+            nn.ConvTranspose2d(8, 4, 4, 2, 1, bias=False)
+            # Removed sigmoid for now
+        )
+            
     def forward(self, x):
-        x = F.relu(self.conv1_bn(self.conv1(x)))
-        x = F.relu(self.conv2_bn(self.conv2(x)))
-        x = F.relu(self.conv3_bn(self.conv3(x)))
-        x = F.relu(self.conv4_bn(self.conv4(x)))
-        x = F.sigmoid(self.conv5(x))
+        x = self.conv(x)
         return x
 
 
@@ -38,28 +55,46 @@ class Discriminator(nn.Module):
 
     def __init__(self):
         super(Discriminator, self).__init__()
-        # Convolutional Layer: 4x16x32 -> 64x8x16, F=4, S=2, P=1
-        self.conv1 = nn.Conv2d(4, 64, 4, 2, 1, bias=False)
+        self.conv = nn.Sequential(
+            # Convolutional Layer: 4x128x256 -> 8x64x128, F=4, S=2, P=1
+            nn.Conv2d(4, 8, 4, 2, 1, bias=False),
+            nn.LeakyReLU(),
 
-        # Convolutional Layer: 64x8x16 -> 128x4x8, F=4, S=2, P=1
-        self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
-        self.conv2_bn = nn.BatchNorm2d(128)
+            # Convolutional Layer: 8x64x128 -> 16x32x64, F=4, S=2, P=1
+            nn.Conv2d(8, 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
 
-        # Convolutional Layer: 128x4x8 -> 256x2x4, F=4, S=2, P=1
-        self.conv3 = nn.Conv2d(128, 256, 4, 2, 1, bias=False)
-        self.conv3_bn = nn.BatchNorm2d(256)
+            # Convolutional Layer: 16x32x64 -> 32x16x32, F=4, S=2, P=1
+            nn.Conv2d(16, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
 
-        # Convolutional Layer: 256x2x4 -> 512x1x2, F=4, S=2, P=1
-        self.conv4 = nn.Conv2d(256, 512, 4, 2, 1, bias=False)
-        self.conv4_bn = nn.BatchNorm2d(512)
+            # Convolutional Layer: 32x16x32 -> 64x8x16, F=4, S=2, P=1
+            nn.Conv2d(32, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
 
-        # Convolutional Layer: 512x1x2 -> 1x1x1, F=(1,2), S=1, P=0
-        self.conv5 = nn.Conv2d(512, 1, (1, 2), 1, 0, bias=False)
+            # Convolutional Layer: 64x8x16 -> 128x4x8, F=4, S=2, P=1
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            # Convolutional Layer: 128x4x8 -> 256x2x4, F=4, S=2, P=1
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+
+            # Convolutional Layer: 256x2x4 -> 512x1x2, F=4, S=2, P=1
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+
+            # Convolutional Layer: 512x1x2 -> 1x1x1, F=(1,2), S=1, P=0
+            nn.Conv2d(512, 1, (1, 2), 1, 0, bias=False),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), negative_slope=0.2)
-        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), negative_slope=0.2)
-        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), negative_slope=0.2)
-        x = F.leaky_relu(self.conv4_bn(self.conv4(x)), negative_slope=0.2)
-        x = F.sigmoid(self.conv5(x))
+        x = self.conv(x)
         return x
